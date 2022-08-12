a45cdf13303963fc4c6538ea2d13a9c7c8b44eab
Alexander Korotkcevitch <65502660+QI-ak@users.noreply.github.com>
2022-08-12 13:30:43 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// swiper
+const swiper = new Swiper('.hero__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
